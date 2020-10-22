bc8f1209cf406fa0b23ff604cf7021ac6120a94d
cmmagalhaes9 <72709491+cmmagalhaes9@users.noreply.github.com>
2020-10-22 12:32:25 +0100

    Update cleanmembers.js

--- commands/cleanmembers.js
+++ commands/cleanmembers.js
@@ -5,6 +5,6 @@ module.exports = {
     description:'limpar inativos',
     execute(message){
         guild.members.prune({days: 7, reason:'Inativo à 30 dias, para voltar a entrar no servidor pede um convite e não esqueças de pedir roles na próxima!'});
-            
+        message.Channel.send('clean');
     }
 }
