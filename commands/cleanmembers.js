faaa0e3911e9b18c09b657def8065b66c10850e8
cmmagalhaes9 <72709491+cmmagalhaes9@users.noreply.github.com>
2020-10-22 14:01:06 +0100

    Update cleanmembers.js

--- commands/cleanmembers.js
+++ commands/cleanmembers.js
@@ -3,6 +3,7 @@ module.exports = {
     name:'cleanmembers',
     description:'limpar inativos',
     execute(message){
+        const guild = client.guilds.cache.get("537337830138052615");
         guild.members.cache.forEach(member => console.log(member.user.username));
     }
 }
