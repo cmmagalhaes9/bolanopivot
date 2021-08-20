cd48eebe5f6041607fd5ad7286f5afe55785adb7
cmmagalhaes9 <72709491+cmmagalhaes9@users.noreply.github.com>
2021-08-20 23:26:25 +0100

    Delete up.js

--- commands/up.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name:'info',
-    description:'informar users',
-    execute(message){
-        console.log('ok');
-        message.channel.send("Bola No Pivot está novamente online!");
-    }
-}
-
