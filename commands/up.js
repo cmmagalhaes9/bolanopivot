25bca4fc7abf6a6c029fc3952cea58eb388614d7
cmmagalhaes9 <72709491+cmmagalhaes9@users.noreply.github.com>
2021-08-20 23:13:11 +0100

    Create up.js

---
+++ commands/up.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name:'up',
+    description:'informar up',
+   
+        console.log('ok');
+        message.channel.send("Bola No Pivot está novamente online!");
+    }
+}
