e0aa1abc1b74960e99d83692b2d8d601c5f5eee2
cmmagalhaes9 <cmmagalhaes9@hotmail.com>
2022-09-13 23:23:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -22,38 +22,38 @@ for(const file of commandFiles)
 client.on('ready', ()=>{
     var channel = client.channels.cache.find(c=>c.id==='764890387668008990');
     console.log("Bola no pivot está online!");
-    //channel.send("Bola No Pivot está novamente online!");
+    channel.send("Bola No Pivot está novamente online!");
 })
 
-// client.on('message',message=>{
-//     if(!message.content.startsWith(prefix) || message.author.bot)return;
+client.on('message',message=>{
+    if(!message.content.startsWith(prefix) || message.author.bot)return;
 
-//     const args= message.content.slice(prefix.length).trim().split(' ');
-//     const command = args.shift().toLowerCase();
-//     console.log(command)
+    const args= message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+    console.log(command)
 
-//     if(message.channel.id=='708666739872890950' || message.channel.id=='764890387668008990')
-//     {
+    if(message.channel.id=='708666739872890950' || message.channel.id=='764890387668008990')
+    {
        
-//         if(command==='study'){
-//         client.commands.get('study').execute(message,args);   
-//         };
-//         if(command==='unstudy'){
-//             client.commands.get('unstudy').execute(message,args) 
-//         };
-//         if(command==='info'){
-//             console.log("AQUI")
-//             client.commands.get('info').execute(message)
-//         };
-//         if(command==='ucs'){
-//             client.commands.get('ucs').execute(message)
-//         };
+        if(command==='study'){
+        client.commands.get('study').execute(message,args);   
+        };
+        if(command==='unstudy'){
+            client.commands.get('unstudy').execute(message,args) 
+        };
+        if(command==='info'){
+            console.log("AQUI")
+            client.commands.get('info').execute(message)
+        };
+        if(command==='ucs'){
+            client.commands.get('ucs').execute(message)
+        };
         
         
 
-//     }
+    }
     
-// });
+});
 
 const token1='NzY0MjE3OTc5MDkyMjA1NjA5'
 const token2='.GxHVwB.'
